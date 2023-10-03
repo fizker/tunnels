5aa734015e933153d11bbc0e0dc7b43927d04d68
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-10-04 00:59:55 +0200

    Added AsyncHTTPClient dependency

--- tunnels-client/Package.swift
+++ tunnels-client/Package.swift
@@ -7,9 +7,15 @@ let package = Package(
 	products: [
 		.executable(name: "tunnels", targets: ["CLI"])
 	],
+	dependencies: [
+		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
+	],
 	targets: [
 		.target(
-			name: "Tunnels"
+			name: "Tunnels",
+			dependencies: [
+				.product(name: "AsyncHTTPClient", package: "async-http-client"),
+			]
 		),
 		.executableTarget(
 			name: "CLI",
