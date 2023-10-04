0a364d5b0ff1e77ccbfb0edfc57e148099a6bb7a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-10-04 02:38:38 +0200

    Added support for client to connect to WebSocket

--- tunnels-client/Package.swift
+++ tunnels-client/Package.swift
@@ -4,18 +4,21 @@ import PackageDescription
 
 let package = Package(
 	name: "tunnels-client",
-	platforms: [ .macOS(.v10_15) ],
+	platforms: [ .macOS(.v13) ],
 	products: [
 		.executable(name: "tunnels", targets: ["CLI"])
 	],
 	dependencies: [
 		.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
+		.package(url: "https://github.com/vapor/websocket-kit.git", from: "2.14.0"),
+
 	],
 	targets: [
 		.target(
 			name: "Tunnels",
 			dependencies: [
 				.product(name: "AsyncHTTPClient", package: "async-http-client"),
+				.product(name: "WebSocketKit", package: "websocket-kit"),
 			]
 		),
 		.executableTarget(
--- tunnels-client/Sources/Tunnels/tunnels_client.swift
+++ tunnels-client/Sources/Tunnels/tunnels_client.swift
@@ -1,5 +1,7 @@
 import AsyncHTTPClient
 import Foundation
+import NIO
+import WebSocketKit
 
 public func hello() async throws -> String {
 	let client = HTTPClient()
@@ -7,6 +9,15 @@ public func hello() async throws -> String {
 	let response = try await client.get(url: "http://localhost:8110/hello")
 	let body = try await response.body.collect(upTo: 1024 * 1024)
 	return String(data: Data(buffer: body), encoding: .utf8) ?? ""
+}
+
+public func connect(id: UUID) async throws {
+	let elg = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+	try await WebSocket.connect(to: "ws://localhost:8110/tunnels/\(id)", on: elg) { ws in
+		ws.onText { ws, value in
+			print("From server: \(value)")
+		}
+	}.get()
 }
 
 extension HTTPClient {
