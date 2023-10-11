3be2b948407a655b7a1228b3ff408468095ff907
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-10-12 00:05:04 +0200

    Added convenience init for Bit for counting position from the left instead of the right

--- Sources/Binary/Bit.swift
+++ Sources/Binary/Bit.swift
@@ -2,7 +2,7 @@ public enum Bit {
 	case zero
 	case one
 
-	public init<T: BinaryInteger>(_ value: T, at position: Int) {
+	public init<T: BinaryInteger>(_ value: T, position: Int) {
 		guard position >= 0 && position < value.bitWidth
 		else {
 			self = .zero
@@ -13,6 +13,10 @@ public enum Bit {
 		let truncated = shifted & 1
 
 		self = truncated == 0 ? .zero : .one
+	}
+
+	public init<T: BinaryInteger>(_ value: T, positionFromLeft: Int) {
+		self.init(value, position: value.bitWidth - positionFromLeft - 1)
 	}
 
 	public var value: UInt8 {
--- Tests/BinaryTests/BitTests.swift
+++ Tests/BinaryTests/BitTests.swift
@@ -5,13 +5,26 @@ final class BitTests: XCTestCase {
 	func test__initWithBinaryInteger__valueIsUInt8_positionIsValid__correctBitValueReturned() async throws {
 		let value: UInt8 = 0b01010110
 
-		XCTAssertEqual(Bit(value, at: 7), .zero)
-		XCTAssertEqual(Bit(value, at: 6), .one)
-		XCTAssertEqual(Bit(value, at: 5), .zero)
-		XCTAssertEqual(Bit(value, at: 4), .one)
-		XCTAssertEqual(Bit(value, at: 3), .zero)
-		XCTAssertEqual(Bit(value, at: 2), .one)
-		XCTAssertEqual(Bit(value, at: 1), .one)
-		XCTAssertEqual(Bit(value, at: 0), .zero)
+		XCTAssertEqual(Bit(value, position: 7), .zero)
+		XCTAssertEqual(Bit(value, position: 6), .one)
+		XCTAssertEqual(Bit(value, position: 5), .zero)
+		XCTAssertEqual(Bit(value, position: 4), .one)
+		XCTAssertEqual(Bit(value, position: 3), .zero)
+		XCTAssertEqual(Bit(value, position: 2), .one)
+		XCTAssertEqual(Bit(value, position: 1), .one)
+		XCTAssertEqual(Bit(value, position: 0), .zero)
+	}
+
+	func test__initWithBinaryIntegerFromLeft__valueIsUInt8_positionIsValid__correctBitValueReturned() async throws {
+		let value: UInt8 = 0b01010110
+
+		XCTAssertEqual(Bit(value, positionFromLeft: 0), .zero)
+		XCTAssertEqual(Bit(value, positionFromLeft: 1), .one)
+		XCTAssertEqual(Bit(value, positionFromLeft: 2), .zero)
+		XCTAssertEqual(Bit(value, positionFromLeft: 3), .one)
+		XCTAssertEqual(Bit(value, positionFromLeft: 4), .zero)
+		XCTAssertEqual(Bit(value, positionFromLeft: 5), .one)
+		XCTAssertEqual(Bit(value, positionFromLeft: 6), .one)
+		XCTAssertEqual(Bit(value, positionFromLeft: 7), .zero)
 	}
 }
