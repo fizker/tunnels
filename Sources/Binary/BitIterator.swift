09bf9041531e7db4a0db59c0592f24f6106e046b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-10-12 00:22:25 +0200

    Added convenience iterator for asking for any number of bits at a time

--- Sources/Binary/BitIterator.swift
+++ Sources/Binary/BitIterator.swift
@@ -32,4 +32,17 @@ public struct BitIterator<Number: BinaryInteger>: IteratorProtocol {
 
 		return bit
 	}
+
+	public mutating func next(_ count: Int) -> UInt? {
+		var value: UInt = 0
+
+		for position in (0..<count).reversed() {
+			guard let next = next()
+			else { return nil }
+
+			value |= UInt(next.value) << position
+		}
+
+		return value
+	}
 }
--- Tests/BinaryTests/BitIteratorTests.swift
+++ Tests/BinaryTests/BitIteratorTests.swift
@@ -42,4 +42,32 @@ final class BitIteratorTests: XCTestCase {
 
 		XCTAssertNil(iterator.next())
 	}
+
+	func test__nextX__backedByUInt8__returnsExpectedValues() async throws {
+		let value: UInt8 = 0b00101100
+		var iterator = BitIterator(value)
+
+		XCTAssertEqual(iterator.next(2), 0)
+		XCTAssertEqual(iterator.next(2), 2)
+		XCTAssertEqual(iterator.next(2), 3)
+		XCTAssertEqual(iterator.next(2), 0)
+		XCTAssertNil(iterator.next(2))
+
+		iterator = BitIterator(value)
+
+		XCTAssertEqual(iterator.next(3), 1)
+		XCTAssertEqual(iterator.next(3), 3)
+		// This overflows, which kills the rest of the values
+		XCTAssertNil(iterator.next(3))
+		XCTAssertNil(iterator.next())
+
+		iterator = BitIterator(value)
+
+		XCTAssertEqual(iterator.next(), .zero)
+		XCTAssertEqual(iterator.next(3), 2)
+		XCTAssertEqual(iterator.next(3), 6)
+		// This overflows, which kills the rest of the values
+		XCTAssertNil(iterator.next(3))
+		XCTAssertNil(iterator.next())
+	}
 }
