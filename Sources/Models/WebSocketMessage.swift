9047bbdecf3333446d9dabef923ec9da753c38ec
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-10-22 18:58:16 +0200

    tmp websocket-client premature types

--- Sources/Models/WebSocketMessage.swift
+++ Sources/Models/WebSocketMessage.swift
@@ -1,8 +1,6 @@
 /// A message sent from the Client via WebSocket.
 public enum WebSocketClientMessage: Codable {
 	case response(HTTPResponse)
-	case addTunnel(TunnelConfiguration)
-	case removeTunnel(host: String)
 }
 
 /// A message sent from the Server via WebSocket.
