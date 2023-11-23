ffa06a9d7e5b060edbac44de4296d212e5868348
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-11-23 01:40:42 +0100

    Added simple test server for serving as a redirect-target during testing

---
+++ test-server.js
@@ -0,0 +1,37 @@
+/// This is a basic test server for receiving any request and logging the response.
+
+const http = require('http')
+const { pipeline } = require('node:stream/promises')
+
+const port = process.env.PORT ?? 8080
+
+const server = http.createServer(async (req, res) => {
+	const { url, method, headers } = req
+	const body = await readBody(req)
+	console.log({ url, method, headers, body })
+
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'text/plain')
+	res.end('Hello World at ' + req.url)
+})
+
+server.listen(port, () => {
+	console.log(`Server running at port ${port}`)
+})
+
+async function readBody(req) {
+	if(!req.headers['content-type']) {
+		return null
+	}
+
+	let content = ''
+	await pipeline(req, async function* (source) {
+		source.setEncoding('utf8')
+		for await (const chunk of source) {
+			content += chunk
+			yield
+		}
+	})
+
+	return content
+}
