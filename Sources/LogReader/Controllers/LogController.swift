08673d181c6ed56db29ef3bc88323c72b64a4f61
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-11-26 12:35:52 +0100

    The main page now shows the summary list

--- Sources/LogReader/Controllers/LogController.swift
+++ Sources/LogReader/Controllers/LogController.swift
@@ -9,12 +9,42 @@ class LogController {
 	}
 
 	func summaries(req: Request) async -> Response {
-		Response(
-			headers: ["content-type": "text/html"],
-			body: """
+		let summaries = await storage.summaries
+
+		return Response(
+			headers: [
+				"content-type": "text/html"
+			],
+			body: .init(string: """
 			<!doctype html>
-			Hello world!
-			"""
+
+			<h1>Logs</h1>
+			\(summaries.isEmpty
+				? "<p>No logs</p>"
+				: """
+				<table>
+					<tr>
+						<th>Method</th>
+						<th>Path</th>
+						<th>Status</th>
+						<th>Host</th>
+					</tr>
+					\(summaries.map(map).joined())
+				</table>
+				"""
+			)
+			""")
 		)
+	}
+
+	func map(_ summary: LogSummary) -> String {
+		return """
+			<tr>
+				<td>\(summary.requestMethod)</td>
+				<td>\(summary.path)</td>
+				<td>\(summary.responseStatus)</td>
+				<td>\(summary.host)</td>
+			</tr>
+		"""
 	}
 }
