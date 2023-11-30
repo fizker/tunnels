e2734560fe7f9ce83902a8990e057587f6ed2e22
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-11-30 12:40:02 +0100

    Added more tests regarding upserting users

--- Tests/TunnelServerTests/Controllers/UserControllerTests.swift
+++ Tests/TunnelServerTests/Controllers/UserControllerTests.swift
@@ -44,6 +44,88 @@ final class UserControllerTests: XCTestCase {
 		XCTAssertEqual(updatedUsers, users)
 	}
 
+	func test__upsertUser__updatingExistingUser_passwordIsMissing_scopeMissing_usernameIsNotChanged__userIsUnchanged() async throws {
+		let userStore = UserStore()
+
+		let user = User(username: "foo", password: "bar", scopes: [.admin])
+		try await userStore.upsert(user: user, oldUsername: "foo")
+
+		let users = await userStore.users()
+		let request = try upsertUserRequest(username: "foo", scopes: nil, password: nil)
+
+		let controller = try UserController(request: request, userStore: userStore)
+
+		let result = try await controller.upsertUser(usernameParam: usernameParam)
+
+		XCTAssertEqual(user, result)
+
+		let updatedUsers = await userStore.users()
+		XCTAssertEqual(users, updatedUsers)
+	}
+
+	func test__upsertUser__updatingExistingUser_passwordIsDifferent_scopeIsDifferent_usernameIsNotChanged__userIsUpdated() async throws {
+		let userStore = UserStore()
+		var users = await userStore.users()
+
+		let user = User(username: "foo", password: "bar", scopes: [.admin])
+		try await userStore.upsert(user: user, oldUsername: "foo")
+
+		let request = try upsertUserRequest(username: "foo", scopes: [], password: "baz")
+
+		let controller = try UserController(request: request, userStore: userStore)
+
+		let result = try await controller.upsertUser(usernameParam: usernameParam)
+
+		XCTAssertEqual(User(username: "foo", password: "baz"), result)
+		users.append(result)
+
+		let updatedUsers = await userStore.users()
+		XCTAssertEqual(users, updatedUsers)
+	}
+
+	func test__upsertUser__updatingExistingUser_passwordIsMissing_scopeMissing_usernameIsChanged_usernameIsNotColliding__userIsUpdated() async throws {
+		let userStore = UserStore()
+		var users = await userStore.users()
+
+		let user = User(username: "foo", password: "bar", scopes: [.admin])
+		try await userStore.upsert(user: user, oldUsername: "foo")
+
+		let request = try upsertUserRequest(username: "foo2", scopes: nil, password: nil, oldUsername: "foo")
+
+		let controller = try UserController(request: request, userStore: userStore)
+
+		let result = try await controller.upsertUser(usernameParam: usernameParam)
+
+		XCTAssertEqual(User(username: "foo2", password: "bar", scopes: [.admin]), result)
+
+		users.append(result)
+		let updatedUsers = await userStore.users()
+		XCTAssertEqual(users, updatedUsers)
+	}
+
+	func test__upsertUser__updatingExistingUser_passwordIsMissing_scopeMissing_usernameIsChanged_usernameIsColliding__errorThrown_userIsNotUpdated() async throws {
+		let userStore = UserStore()
+
+		let user = User(username: "foo", password: "bar", scopes: [.admin])
+		try await userStore.upsert(user: user, oldUsername: "foo")
+		try await userStore.upsert(user: User(username: "foo2", password: "baz"), oldUsername: "foo2")
+
+		let users = await userStore.users()
+
+		let request = try upsertUserRequest(username: "foo2", scopes: nil, password: nil, oldUsername: "foo")
+
+		let controller = try UserController(request: request, userStore: userStore)
+
+		do {
+			_ = try await controller.upsertUser(usernameParam: usernameParam)
+			XCTFail("Expected a throw")
+		} catch UserStore.Error.usernameExists {
+		}
+
+		let updatedUsers = await userStore.users()
+		XCTAssertEqual(users, updatedUsers)
+	}
+
 	func upsertUserRequest(username: String, scopes: Set<User.Scope>?, password: String?, oldUsername: String? = nil) throws -> Request {
 		let upsertRequest = UpsertUserRequest(username: username, scopes: scopes, password: password)
 		return try request(method: .PUT, body: upsertRequest, parameters: [usernameParam: oldUsername ?? username])
