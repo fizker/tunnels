667392c59af433d049495eaf569a36fd1c419fc6
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-11-30 12:10:58 +0100

    Normalized test code a bit

--- Tests/TunnelServerTests/Controllers/UserControllerTests.swift
+++ Tests/TunnelServerTests/Controllers/UserControllerTests.swift
@@ -3,35 +3,34 @@ import XCTest
 import Vapor
 
 final class UserControllerTests: XCTestCase {
+	let usernameParam = "username"
 	let adminUser = User(username: "admin", password: "1234", scopes: [.admin])
 
 	func test__upsertUser__insertingNewUser_passwordPresent_scopeMissing_usernameIsNotColliding__userIsInserted() async throws {
-		let upsertRequest = UpsertUserRequest(username: "foo", password: "bar")
-		let testStore = UserStore()
-		var users = await testStore.users()
+		let userStore = UserStore()
+		var users = await userStore.users()
 
-		let request = try request(method: .PUT, body: upsertRequest, parameters: ["username": "foo"])
+		let request = try upsertUserRequest(username: "foo", scopes: nil, password: "bar")
 
-		let controller = try UserController(request: request, userStore: testStore)
-		let result = try await controller.upsertUser(usernameParam: "username")
+		let controller = try UserController(request: request, userStore: userStore)
+		let result = try await controller.upsertUser(usernameParam: usernameParam)
 
 		XCTAssertEqual(result, User(username: "foo", password: "bar", scopes: []))
 		users.append(result)
-		let updatedUsers = await testStore.users()
+		let updatedUsers = await userStore.users()
 		XCTAssertEqual(updatedUsers, users)
 	}
 
 	func test__upsertUser__insertingNewUser_passwordIsMissing_scopeMissing_usernameIsNotColliding__errorThrown_userIsNotInserted() async throws {
-		let upsertRequest = UpsertUserRequest(username: "foo", password: nil)
-		let testStore = UserStore()
-		let users = await testStore.users()
+		let userStore = UserStore()
+		let users = await userStore.users()
 
-		let request = try request(method: .PUT, body: upsertRequest, parameters: ["username": "foo"])
+		let request = try upsertUserRequest(username: "foo", scopes: nil, password: nil)
 
-		let controller = try UserController(request: request, userStore: testStore)
+		let controller = try UserController(request: request, userStore: userStore)
 
 		do {
-			_ = try await controller.upsertUser(usernameParam: "username")
+			_ = try await controller.upsertUser(usernameParam: usernameParam)
 			XCTFail("Expected a throw")
 		} catch {
 			guard let error = error as? AbortError
@@ -41,8 +40,13 @@ final class UserControllerTests: XCTestCase {
 			XCTAssertEqual(error.reason, "New users must have a password")
 		}
 
-		let updatedUsers = await testStore.users()
+		let updatedUsers = await userStore.users()
 		XCTAssertEqual(updatedUsers, users)
+	}
+
+	func upsertUserRequest(username: String, scopes: Set<User.Scope>?, password: String?, oldUsername: String? = nil) throws -> Request {
+		let upsertRequest = UpsertUserRequest(username: username, scopes: scopes, password: password)
+		return try request(method: .PUT, body: upsertRequest, parameters: [usernameParam: oldUsername ?? username])
 	}
 
 	func request(method: HTTPMethod, body: any Encodable, parameters: [String: String] = [:]) throws -> Request {
