4a80301d661d44558c6b4850d68cbd705dafa289
Benjamin Horsleben <benjamin@fizkerinc.dk>
2023-12-25 17:31:23 +0100

    Updated server to respect the transfer-encoding/content-length of the response

--- Sources/TunnelServer/Extensions/Model+VaporResponse.swift
+++ Sources/TunnelServer/Extensions/Model+VaporResponse.swift
@@ -2,7 +2,30 @@ import Models
 import Vapor
 
 extension Models.HTTPResponse {
-	var asVaporResponse: Response {
+	private var vaporBody: Response.Body {
+		if headers.firstHeader(named: "content-length") == nil {
+			// do stream
+			let buffer: ByteBuffer? = switch self.body {
+			case let .text(value):
+				ByteBuffer(string: value)
+			case let .binary(data):
+				ByteBuffer(data: data)
+			case .none:
+				nil
+			}
+
+			return .init(stream: { writer in
+				if let buffer {
+					_ = writer.write(.buffer(buffer))
+					.map { _ in
+						writer.write(.end)
+					}
+				} else {
+					_ = writer.write(.end)
+				}
+			}, count: -1)
+		}
+
 		let body: Response.Body = switch self.body {
 		case let .text(value):
 			.init(string: value)
@@ -12,10 +35,19 @@ extension Models.HTTPResponse {
 			.empty
 		}
 
+		return body
+	}
+
+	var asVaporResponse: Response {
+		var headers = self.headers
+
+		headers.removeAll(named: "connection")
+		headers.removeAll(named: "date")
+
 		return Response(
 			status: .custom(code: status.code, reasonPhrase: status.reason),
-			headers: .init(self.headers.map { ($0, $1.joined(separator: " ")) }),
-			body: body
+			headers: .init(headers.map { ($0, $1.joined(separator: " ")) }),
+			body: vaporBody
 		)
 	}
 }
