4be5e281272e3668ce0b58180bd7c16a4981a286
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-01-01 15:49:33 +0100

    Added convenience-class for rejecting a task after a timeout

---
+++ Sources/TunnelClient/TimedResolution.swift
@@ -0,0 +1,41 @@
+actor TimedResolution {
+	enum Result {
+		case resolved, timedOut
+
+		var isSuccess: Bool {
+			switch self {
+			case .resolved: true
+			case .timedOut: false
+			}
+		}
+	}
+
+	private(set) var isResolved = false
+	let timeout: Duration
+	let onEnd: (Result) -> ()
+
+	init(timeout: Duration, onEnd: @escaping (Result) -> Void) {
+		self.timeout = timeout
+		self.onEnd = onEnd
+
+		Task {
+			try await Task.sleep(for: timeout)
+			await self.timeOut()
+		}
+	}
+
+	func resolve() {
+		resolve(with: .resolved)
+	}
+
+	func timeOut() {
+		resolve(with: .timedOut)
+	}
+
+	private func resolve(with result: Result) {
+		guard !isResolved
+		else { return }
+		isResolved = true
+		onEnd(result)
+	}
+}
