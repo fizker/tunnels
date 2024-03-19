93faac31ec32db253735889b6e3612dce3e7fd80
Benjamin Horsleben <benjamin@fizkerinc.dk>
2024-03-19 12:32:36 +0100

    Updated Deferred and tests to cover more scenarios

--- Sources/TunnelClient/Deferred.swift
+++ Sources/TunnelClient/Deferred.swift
@@ -3,33 +3,52 @@ public enum DeferredError: Error, Sendable {
 }
 
 public class Deferred<T> {
+	private enum State {
+		case resolved(T)
+		case rejected
+	}
+
+	private var state: State?
 	private var stream: AsyncStream<T>
 	private var cont: AsyncStream<T>.Continuation
 
 	public init(becoming type: T.Type = T.self) {
-		var cont: AsyncStream<T>.Continuation!
-		self.stream = AsyncStream {
-			cont = $0
-		}
-		self.cont = cont
+		let v = AsyncStream.makeStream(of: T.self)
+		self.stream = v.stream
+		self.cont = v.continuation
 	}
 
 	public func resolve(_ value: T) {
+		guard state == nil
+		else { return }
+
+		state = .resolved(value)
 		cont.yield(value)
 		cont.finish()
 	}
 
 	public func reject() {
+		guard state == nil
+		else { return }
+
+		state = .rejected
 		cont.finish()
 	}
 
 	public var value: T {
 		get async throws {
-			for await value in stream {
+			switch state {
+			case let .resolved(value):
 				return value
-			}
+			case .rejected:
+				throw DeferredError.rejected
+			case nil:
+				for await value in stream {
+					return value
+				}
 
-			throw DeferredError.rejected
+				throw DeferredError.rejected
+			}
 		}
 	}
 }
--- Tests/TunnelClientTests/DeferredTests.swift
+++ Tests/TunnelClientTests/DeferredTests.swift
@@ -2,6 +2,18 @@ import XCTest
 @testable import TunnelClient
 
 final class DeferredTests: XCTestCase {
+	actor Events {
+		var events: [String]
+
+		init(_ events: [String]) {
+			self.events = events
+		}
+
+		func append(_ value: String) {
+			events.append(value)
+		}
+	}
+
 	func test__resolve__singleResolve_syncResolve__resolvesAsExpected() async throws {
 		let deferred = Deferred(becoming: String.self)
 
@@ -16,6 +28,22 @@ final class DeferredTests: XCTestCase {
 		XCTAssertEqual(events, ["start", "resolving", "after await"])
 	}
 
+	func test__value__singleResolve_syncResolve_multipleValue__returnsTheSameValueEveryTime() async throws {
+		let deferred = Deferred(becoming: String.self)
+
+		var events = ["start"]
+
+		events.append("resolving")
+		deferred.resolve("foo")
+
+		let firstActual = try await deferred.value
+		let secondActual = try await deferred.value
+		events.append("after await")
+		XCTAssertEqual("foo", firstActual)
+		XCTAssertEqual("foo", secondActual)
+		XCTAssertEqual(events, ["start", "resolving", "after await"])
+	}
+
 	func test__resolve__multipleResolve_syncResolve__resolvesAsExpected() async throws {
 		let deferred = Deferred(becoming: String.self)
 
@@ -25,9 +53,11 @@ final class DeferredTests: XCTestCase {
 		deferred.resolve("foo")
 		deferred.resolve("bar")
 
-		let actual = try await deferred.value
+		let firstActual = try await deferred.value
+		let secondActual = try await deferred.value
 		events.append("after await")
-		XCTAssertEqual("foo", actual)
+		XCTAssertEqual("foo", firstActual)
+		XCTAssertEqual("foo", secondActual)
 		XCTAssertEqual(events, ["start", "resolving", "after await"])
 	}
 
@@ -100,5 +130,47 @@ final class DeferredTests: XCTestCase {
 		}
 
 		XCTAssertEqual(events, ["start", "resolving", "error caught"])
+	}
+
+	func test__resolve__valueRequestedBeforeResolve__resolvesAsExpected() async throws {
+		let deferred = Deferred(becoming: String.self)
+
+		let events = Events(["start"])
+
+		Task {
+			try await Task.sleep(for: .milliseconds(100))
+			await events.append("resolving")
+			deferred.resolve("foo")
+		}
+
+		await events.append("requesting value")
+		_ = try await deferred.value
+		await events.append("value fetched")
+
+		let e = await events.events
+		XCTAssertEqual(e, ["start", "requesting value", "resolving", "value fetched"])
+	}
+
+	func test__reject__valueRequestedBeforeRejection__resolvesAsExpected() async throws {
+		let deferred = Deferred(becoming: String.self)
+
+		let events = Events(["start"])
+
+		Task {
+			try await Task.sleep(for: .milliseconds(100))
+			await events.append("resolving")
+			deferred.reject()
+		}
+
+		await events.append("requesting value")
+		do {
+			_ = try await deferred.value
+			XCTFail()
+		} catch DeferredError.rejected {
+			await events.append("error caught")
+		}
+
+		let e = await events.events
+		XCTAssertEqual(e, ["start", "requesting value", "resolving", "error caught"])
 	}
 }
